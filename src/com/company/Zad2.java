1a7fac337b5a4e5b849f8d578132f6f6aa9c9ca4
Adrian Langier <langier.adrian@gmail.com>
2021-03-22 14:14:48 +0100

    solved Task2

--- src/com/company/Zad2.java
+++ src/com/company/Zad2.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Zad2 {
+    static Scanner scanner = new Scanner(System.in);
+
+    static int substract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Podaj 2 liczby");
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("Wynik odejmowania to: " + substract(firstNumber, secondNumber));
+
+    }
+
+
 }
