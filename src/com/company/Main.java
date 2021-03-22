d7ac7927284d53df1b1bb26e3cd12e289ab41ae4
Adrian Langier <langier.adrian@gmail.com>
2021-03-22 14:04:38 +0100

    solved Task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Podaj 2 liczby");
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("Wynik dodawania to: " + add(firstNumber, secondNumber));
     }
+
+
 }
