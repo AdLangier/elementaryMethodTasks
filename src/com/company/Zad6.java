9e1dcfd3236ce78a27a7e96e8f47d295e6a7dcff
Adrian Langier <langier.adrian@gmail.com>
2021-03-25 10:50:49 +0100

    solved Task6

---
+++ src/com/company/Zad6.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Zad6 {
+    static Scanner scanner = new Scanner(System.in);
+
+    static int digitsSum(int n)                           // używamy pętli "while" ponieważ nie wiemy ile cyfr będzie miała liczba podana przez użytkownika
+    {
+        int sum = 0;                                      // niestety ze względu na użycie typu "int" program jest ograniczony
+        while (n != 0)                                    // do maksymalnej wartości "inta" przy użyciu double był problem
+        {                                                 // z formatowaniem
+            sum = (sum + n % 10);
+            n = n/10;                                     //WAŻNE rozpisać na czynniki pierwsze kolejność działania warunku w pętli
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Podaj liczbę");
+        int number = scanner.nextInt();
+        System.out.println("Suma cyfr tej liczby to: "+digitsSum(number));
+    }
+}
